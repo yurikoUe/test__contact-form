949fc85d9f84a36948a217e6d2b876781f83c1af
yuriko <futabagusa.yuriko@gmail.com>
2025-01-09 12:08:27 +0900

    セットアップ完了

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact-form</title>
+    <link rel="stylesheet" href="{{ ('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ ('css/common.css') }}">
+    @yield('css')
+</head>
+<body>
+    <header>
+        
+    </header>
+</body>
+</html>
